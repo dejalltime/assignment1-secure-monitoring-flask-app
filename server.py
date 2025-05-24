ca85ec14974e498a759d296fd0f4d611b06872dd
Jonathan Martins <mart1449@algonquinlive.com>
2025-05-24 17:15:48 -0400

    routes - callback route

--- server.py
+++ server.py
@@ -32,3 +32,9 @@ def login():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
     )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
