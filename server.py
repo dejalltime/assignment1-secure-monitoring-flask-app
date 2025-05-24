f25dc8bc4b37c7892624202b2193cda75342495a
Jonathan Martins <mart1449@algonquinlive.com>
2025-05-24 17:16:10 -0400

    routes - logout route

--- server.py
+++ server.py
@@ -38,3 +38,18 @@ def callback():
     token = oauth.auth0.authorize_access_token()
     session["user"] = token
     return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
